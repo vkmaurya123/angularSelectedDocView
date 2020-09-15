1ba530e2636efbfc5514b6e6b665482968ef70e4
vineet <vineet@codestore.com>
2020-09-15 17:42:19 +0530

    selected doc commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ApiUrls } from './apiUrl';
+import { selectedDocApis   } from './apiUrlsInterface';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'testApp';
+
+  public apiUrlForSelectedDoc : selectedDocApis = {
+    selectedDocView: ApiUrls.getDocumentWithKendoRequest
+   }
+
+   public viewType : string='1';
+
+   public selectedDocId : string = '173';
+
+  public styleConfig = { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { SelectedDocViewModule} from '@vkmaurya123/doclibrary';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditorModule } from '@progress/kendo-angular-editor';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { ToolBarModule } from '@progress/kendo-angular-toolbar';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SelectedDocViewModule, 
+    BrowserAnimationsModule,
+    EditorModule,
+    GridModule,
+    ToolBarModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
